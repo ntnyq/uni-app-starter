09a7dc30111d5783999eb9df4956b61c39a2c7fa
ntnyq <ntnyq13@gmail.com>
2024-07-18 23:36:56 +0800

    chore: update

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,19 @@
-import { defineConfig, presetIcons } from 'unocss'
+import {
+  defineConfig,
+  presetIcons,
+  presetUno,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss'
 
 export default defineConfig({
-  presets: [presetIcons()],
+  presets: [
+    presetUno(),
+    presetIcons({
+      scale: 1.2,
+      extraProperties: {},
+    }),
+  ],
+
+  transformers: [transformerDirectives(), transformerVariantGroup()],
 })
